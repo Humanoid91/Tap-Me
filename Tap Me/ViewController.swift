9dffbf530e34b476be8991bbe023356789e97680
ZopperMAC <zoppermac@ZopperMACs-MacBook-Pro.local>
2014-12-04 13:29:32 +0530

    Tap Me Game Completed

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupGame()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    var count = 0;
+    var seconds = 0;
+    var timer = NSTimer()
+    func setupGame(){
+        seconds = 30
+        count = 0
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+    }
+    func subtractTime(){
+        seconds--
+        timerLabel.text = "Time: \(seconds)"
+        
+        if(seconds == 0){
+            timer.invalidate()
+            let alert = UIAlertController(title: "Time is up!", message: "You scored \(count) points", preferredStyle:UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertActionStyle.Default, handler: {action in self.setupGame()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        count += 1
+        scoreLabel.text="Score \n\(count)"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
